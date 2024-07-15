3c8533c8008d6364f9cdf63aa7be9ca1a8d52992
amandanethmini <140165817+amandanethmini@users.noreply.github.com>
2024-07-15 05:23:40 -0700

    Update practical5_1.scala

--- practical5_1.scala
+++ practical5_1.scala
@@ -1,34 +1,35 @@
 import scala.io.StdIn.readLine
 
 object practical5_1 {
-  def main(args: Array[String]): Unit = {
-    val productList = getProductList()
-    printProductList(productList)
-    val totalProducts = getTotalProducts(productList)
-    println(s"Total number of products: $totalProducts")
-  }
 
   def getProductList(): List[String] = {
     var products: List[String] = List.empty
     var input = ""
-    do {
+    while (input != "done") {
       println("Enter a product name (type 'done' to finish): ")
-      input = readLine().trim
+      input = readLine()
       if (input != "done") {
         products = products :+ input
       }
-    } while (input != "done")
-    products
+    }
+    return products
   }
 
   def printProductList(products: List[String]): Unit = {
     println("Product List:")
-    products.zipWithIndex.foreach { case (product, index) =>
-      println(s"${index + 1}. $product")
+    for (index <- products.indices) {
+      println(s"${index + 1}. ${products(index)}")
     }
   }
 
   def getTotalProducts(products: List[String]): Int = {
     products.length
+  }
+
+  def main(args: Array[String]): Unit = {
+    val productList = getProductList()
+    printProductList(productList)
+    val totalProducts = getTotalProducts(productList)
+    println(s"Total number of products: $totalProducts")
   }
 }
